a43048251ad894ffc632de9edaa922bbbe70a80c
LARA CONVERSO <laraconverso@gmail.com>
2021-01-23 00:26:14 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,15 @@
-function go(){
-    let str = document.getElementById("t1").value;
-    let vocal = "aeiouAEIOU";
-    let consonante = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
-    let ctrV = 0;
-    let ctrC = 0;
-    for(let x = 0; x < str.length; x++){
-        for(let y = 0; y < vocal.length; y++){
-            if(str[x] ==vocal[y])
-            {
-                ctrV++;
-            }
-        }
-        for(let z = 0; z < consonante.length; z++){
-            if(str[x] ==consonante[z])
-            {
-                ctrC++;
-            }
-        }
-    }
-    document.getElementById("p1").innerHTML = "La cantidad de vocales ingresadas es " + ctrV;
-    document.getElementById("p2").innerHTML = "La cantidad de consonantes ingresadas es: " + ctrC;
-} 
+//Carrito
+function MyItem (argNombre){
+    this.nombre = argNombre;
+}
+
+let miCarrito =[];
+const itemsCarrito = 10;
+for (let index = 0; index < Array.length; index++){
+    const element = new MyItem ("ItemNumero" + index)
+    miCarrito.push(element);
+}
+console.log(miCarrito); 
+//Productos 
+
     
