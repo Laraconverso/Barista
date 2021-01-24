a07cb23a7233ff3d9be1af59076ade978ed4592d
LARA CONVERSO <laraconverso@gmail.com>
2021-01-24 18:58:46 -0300

    adding objects

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 //Carrito
-function MyItem (argNombre){
-    this.nombre = argNombre;
+function Item(product, price) {
+    this.product = product;
+    this.price = price;
+    this.buy = function () { return `Se ha agregado ${product} a su carrito.`};
 }
-
-let miCarrito =[];
-const itemsCarrito = 10;
-for (let index = 0; index < Array.length; index++){
-    const element = new MyItem ("ItemNumero" + index)
-    miCarrito.push(element);
-}
-console.log(miCarrito); 
-//Productos 
+//Products
+  let product1 = new Item("Vainilla Brew", "$1000");
+  let product2 = new Item("Shangai Blend", "$1000");
+  let product3 = new Item("Colombian Blend","$1000");
+//visualize (this lines will be eliminated)
+console.log(product1.buy()); 
+console.log(product2.buy());
+console.log(product3.buy());
 
     
