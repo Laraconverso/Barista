63d1c192c794d2b9be8aac45b0691307de43c757
LARA CONVERSO <laraconverso@gmail.com>
2021-01-26 17:47:41 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 //Carrito
-let cart =[];
+let myCart =[];
 let quantity = 0;
 
-function Item(product, price) {
+function Item(num,product, price) {
+    this.num = num;
     this.product = product;
     this.price = price;
     this.buy = function () { return `Se ha agregado ${product} a su carrito.`};
 }
 //Products
-  let product1 = new Item("Vainilla Brew", "$1000");
-  let product2 = new Item("Shangai Blend", "$1000");
-  let product3 = new Item("Colombian Blend","$1000");
-//visualize (this lines will be eliminated)
-console.log(product1.buy()); 
-console.log(product2.buy());
-console.log(product3.buy());
+  let product1 = new Item(0, "Vainilla Brew", "$1000");
+  let product2 = new Item(1, "Shangai Blend", "$1000");
+  let product3 = new Item(2, "Colombian Blend","$1000");
+//Add to cart
+  function addToCart(num){
+    if (quantity > 10 ){
+        myCart.push(Item[num]);
+        quantity = myCart.length;
+        //visualize
+        console.log("Se ha agregado", product, "a su carrito.");
+        console.log(Item.values(product));
+    } else {
+        alert("No hay mas stock");
+    }
+}
+
+
