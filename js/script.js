72afd6e21fe9496dbb9525b1289aea9f24dee995
LARA CONVERSO <laraconverso@gmail.com>
2021-01-26 22:39:35 -0300

    adding arrays

--- js/script.js
+++ js/script.js
@@ -7,24 +7,10 @@ function Item(num, product, price) {
     this.product = product;
     this.price = price;
 }
-
 //Products
   let product1 = new Item(0, "Vainilla Brew", "$1000");
   let product2 = new Item(1, "Shangai Blend", "$1000");
   let product3 = new Item(2, "Colombian Blend","$1000");
-//Add to cart
-  function addToCart(num){
-    if (quantity < 5 ){
-        myCart.push(Item[num]);
-        quantity = myCart.length;
-        //visualize
-        console.log("Se ha agregado", product, "a su carrito.");
-        console.log(Item.values(product));
-    } else {
-        alert("No hay mas stock");
-    }
-}
-
 
 for (let index=0; index < quantity; index++) {
   const element = new Item("Item Numero " + index);
@@ -34,3 +20,14 @@ for (let index=0; index < quantity; index++) {
 }
 //visualize
 console.log(myCart);
+//Add to cart
+function addToCart(num){
+  if (quantity > 10 ){
+      myCart.push(Item[num]);
+      quantity = myCart.length;
+      //visualize
+      console.log(`Se ha agregado ${quantity} a su carrito.`);
+  } else {
+      alert("No hay mas stock");
+  }
+}
