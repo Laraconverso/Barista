ec239fea3564131010bfd741643ee355e352dfc8
LARA CONVERSO <laraconverso@gmail.com>
2021-02-09 18:59:17 -0300

    update JSON stock

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-//Carrito
-let myCart =[];
-let quantity = 1;
-
-function Item(num, product, price) {
-    this.num = num;
-    this.product = product;
-    this.price = price;
-}
-//Products
-  let product1 = new Item(0, "Vainilla Brew", "$1000");
-  let product2 = new Item(1, "Shangai Blend", "$1000");
-  let product3 = new Item(2, "Colombian Blend","$1000");
-
-//Add to cart
-function addToCart(num){
-  if (quantity < 10 ){
-      myCart.push(Item[num]);
-      quantity = myCart.length;
-      //visualize
-      console.log(`Se ha agregado ${quantity} elementos a su carrito.`);
-  } else {
-      console.log("No hay mas stock");
-  }
-}
-
 //Add items to the cart
 let buttonBuyList = document.getElementsByClassName("buttonBuy");
 
@@ -75,3 +49,37 @@ if(localStorage.getItem('dark-mode') === 'true'){
   document.body.classList.remove('dark');
   btnSwitch.classList.remove('active');
 }
+
+
+
+
+
+
+/* COMMENT SECTION (old code)
+ //Carrito
+let myCart =[];
+let quantity = 1;
+
+function Item(num, product, price) {
+    this.num = num;
+    this.product = product;
+    this.price = price;
+}
+
+//Add to cart
+function addToCart(num){
+  if (quantity < 10 ){
+      myCart.push(Item[num]);
+      quantity = myCart.length;
+      //visualize
+      console.log(`Se ha agregado ${quantity} elementos a su carrito.`);
+  } else {
+      console.log("No hay mas stock");
+  }
+}
+//Products
+  let product1 = new Item(0, "Vainilla Brew", "$1000");
+  let product2 = new Item(1, "Shangai Blend", "$1000");
+  let product3 = new Item(2, "Colombian Blend","$1000");
+
+*/
--- js/stock.js
+++ js/stock.js
@@ -13,47 +13,47 @@ let stockProducts = [
      "id" : 3,
      "name": "Budapest Blend",
      "price":"1050"
-  } 
+  }, 
   {
     "id" : 4,
     "name":"Colombia Blend",
     "price":"1050"
-  }    
+  },    
   {
     "id" : 5,
     "name":"Chocolate Blend",
     "price":"1050"
-  } 
+  }, 
   {
     "id" : 6,
     "name":"Ethiopia Blend",
     "price":"1050"
-  } 
+  }, 
   {
     "id" : 7,
     "name":"Capetown Blend",
     "price":"1050"
-  } 
+  }, 
   {
     "id" : 8,
     "name":"Moscow Blend",
     "price":"1050"
-  }
+  },
   {
     "id" : 9,
     "name":"Caramel Blend",
     "price":"1050"
-  }  
+  },  
   {
     "id" : 10,
     "name":" Oslo Brew",
     "price":"1050"
-  } 
+  },
   {
     "id" : 11,
     "name":" Vancouver Brew",
     "price":"1050"
-  } 
+  },
   {
     "id" : 12,
     "name":"Buenos Aires Brew",
